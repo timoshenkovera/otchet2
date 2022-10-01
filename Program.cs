a8ef7397a1242bdb3b3cbaa8a6c5d9a766f28185
timoshenkovera <112927181+timoshenkovera@users.noreply.github.com>
2022-10-01 09:31:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите массив данных через ',': ");
+string strLine = Console.ReadLine();
+string[] inArr = strLine.Split(",");
+string[] outArr = new string[0];
+
+for (int i = 0; i < inArr.Length; i++)
+{
+    if (inArr[i].Length <= 3)
+    {
+        Array.Resize(ref outArr, outArr.Length + 1);
+        outArr[outArr.Length-1] = inArr[i];
+    }
+
+}
+Console.WriteLine($"[{string.Join(",", inArr)}]=>[{string.Join(",", outArr)}]");
